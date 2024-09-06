f39744b9845075a85f8fb7e7d7f3d7cb8e09f3f5
Alvaro <besadaferreralvaro@gmail.com>
2024-09-06 17:07:52 -0400

    AC

---
+++ Constructive Problems.cpp
@@ -0,0 +1,17 @@
+
+
+    #include <bits/stdc++.h>
+     
+    using namespace std;
+     
+    int main()
+    {
+        int cp;
+        cin>>cp;
+        while(cp--){
+            int a, b;
+            cin>>a>>b;
+            cout<<max(a,b)<<"\n";
+        }
+        
+    }
